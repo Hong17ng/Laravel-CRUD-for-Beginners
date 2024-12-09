1d7de11a6d20f46a72665ea9202a8bb89daf5989
Hong17ng <qihongng123456@gmail.com>
2024-12-09 16:32:58 +0800

    1

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -8,7 +8,31 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My App</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container mt-5">
+    <div class="mb-4 mt-5">
+        <a href="{{ url()->previous() }}" class="btn btn-secondary">Back</a>
+    </div>
         <h1 class="mb-4">Create a Product</h1>
 
         <!-- Error messages -->
